1d50bdbc41a8003d20fd44e478d3e01554955634
habtomab <47911370+habtomab@users.noreply.github.com>
2019-05-27 22:27:22 -0500

    created class and some implementations

--- src/softwareProject/MyClass.java
+++ src/softwareProject/MyClass.java
@@ -4,7 +4,12 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("this is my class");
+		System.out.println("this is my class");
+		System.out.println("this is my class");
+
 
 	}
-
+class Habtom{
+		private Habtom atom;
+}
 }
